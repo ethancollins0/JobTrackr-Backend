e0d61a72b2721a83cf56716a2251de56edd4593a
Ethan Collins <ethantcollins98@gmail.com>
2019-11-07 01:16:59 -0700

    working login validation and sends correct data to frontend

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const db = require('./db_queries')
 
 const app = express()
-app.use(cors())
+
 app.use(bodyParser.urlencoded())
 app.use(bodyParser.json())
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.json('hiya, get route')
+})
+
+app.post('/login', (req, res) => {
+    const {email, password} = req.body
+    console.log(email, password)
+    email && password
+        ? db.checkLogin(email, password)
+            .then(user_id => {
+                user_id
+                    ? db.getLists(user_id).then(data => res.json(data))
+                    : res.json(null)
+            })  
+        : res.json(null)
 })
 
 let port = process.env.PORT || 3001
--- seeds/default_user.js
+++ seeds/default_user.js
@@ -1,13 +1,25 @@
+const bcrypt = require('bcrypt')
 
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('list_items').del()
+    .then(() => knex('lists').del())
+    .then(() => knex('users').del())
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
+      return knex('users').insert({name: 'Ethan', 
+                                   email: 'ethan@gmail.com', 
+                                   password: 'password'}).returning('id')
+        .then((id) => {
+          id = id[0]
+          return knex('lists').insert({name: 'Todo', user_id: id}).returning('id')
+            .then((id) => {
+              id = id[0]
+              return knex('list_items').insert([
+                {name: 'First Task', description: 'Gotta do that thing', list_id: id},
+                {name: 'Second Task', description: 'Gotta do that second thing', list_id: id},
+                {name: 'Third Task', description: 'Gotta do that third thing', list_id: id},
+              ])
+            })
+        })
     });
 };
